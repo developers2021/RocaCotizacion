850c9d1f724ba6ddd6a8e02a22a688185581f3ed
Norman Henriquez <84880453+developers2021@users.noreply.github.com>
2024-03-04 16:22:37 -0600

    Metodo Login Creado
    Pendiente transicion al menu

--- app/src/main/java/com/example/rocacotizacion/LoginFragment.kt
+++ app/src/main/java/com/example/rocacotizacion/LoginFragment.kt
@@ -4,8 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.rocacotizacion.R
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
+
+
+
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +28,66 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val loginButton: Button = view.findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            val username = view.findViewById<EditText>(R.id.username).text.toString()
+            val password = view.findViewById<EditText>(R.id.password).text.toString()
+
+            sendLoginRequest(username, password)
+        }
+    }
+    private fun sendLoginRequest(username: String, password: String) {
+        val okHttpClient = OkHttpClient()
+        val json = """{"username":"$username","password":"$password"}"""
+        val requestBody = RequestBody.create("application/json".toMediaTypeOrNull(), json)
+
+
+        val request = Request.Builder()
+            .url(Utilidades.URL_AUTH_LOGIN)
+            .post(requestBody)
+            .build()
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle the error
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (it.isSuccessful) {
+                        val responseBodyString = it.body?.string()
+
+                        // Parsing the JSON response
+                        val json = JSONObject(responseBodyString)
+                        val result = json.getBoolean("result")
+                        val message = json.getString("message")
+
+                        activity?.runOnUiThread {
+                            // Update your UI based on 'result' and 'message'
+                            if (result) {
+                                // Login success
+                                Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+
+                            } else {
+                                // Login failed, show the message to the user
+                                Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    } else {
+                        // Handle the case where the server responded with an error status
+                        activity?.runOnUiThread {
+                            // Update UI to show an error message
+                        }
+                    }
+                }
+            }
+        })
+
+
     }
 }
---
+++ app/src/main/java/com/example/rocacotizacion/Utilidades.kt
@@ -0,0 +1,9 @@
+package com.example.rocacotizacion
+
+class Utilidades {
+    companion object {
+        //pruebas
+        const val URL_WEB_SERVICE = "https://b8b0-2803-7780-6-5380-54c6-ec47-645f-e21d.ngrok-free.app"
+        const val URL_AUTH_LOGIN = "$URL_WEB_SERVICE/UserAuth"
+    }
+}
