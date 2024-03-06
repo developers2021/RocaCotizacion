71ed3de852ff2daae77fb90b4f4e78afa42c59ef
Norman Henriquez <84880453+developers2021@users.noreply.github.com>
2024-03-06 16:59:15 -0600

    Evaluando conexion a internet antes de hacer login

--- app/src/main/java/com/example/rocacotizacion/LoginFragment.kt
+++ app/src/main/java/com/example/rocacotizacion/LoginFragment.kt
@@ -1,5 +1,7 @@
 package com.example.rocacotizacion
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -41,55 +43,66 @@ class LoginFragment : Fragment() {
             sendLoginRequest(username, password)
         }
     }
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
+    }
     private fun sendLoginRequest(username: String, password: String) {
-        val okHttpClient = OkHttpClient()
-        val json = """{"username":"$username","password":"$password"}"""
-        val requestBody = RequestBody.create("application/json".toMediaTypeOrNull(), json)
+
+        if (isOnline(requireContext())) {
+            // Proceed with login process (e.g., make API call)
+            val okHttpClient = OkHttpClient()
+            val json = """{"username":"$username","password":"$password"}"""
+            val requestBody = RequestBody.create("application/json".toMediaTypeOrNull(), json)
 
 
-        val request = Request.Builder()
-            .url(Utilidades.URL_AUTH_LOGIN)
-            .post(requestBody)
-            .build()
+            val request = Request.Builder()
+                .url(Utilidades.URL_AUTH_LOGIN)
+                .post(requestBody)
+                .build()
 
-        okHttpClient.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                // Handle the error
-                e.printStackTrace()
-            }
+            okHttpClient.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    // Handle the error
+                    e.printStackTrace()
+                }
 
-            override fun onResponse(call: Call, response: Response) {
-                response.use {
-                    if (it.isSuccessful) {
-                        val responseBodyString = it.body?.string()
+                override fun onResponse(call: Call, response: Response) {
+                    response.use {
+                        if (it.isSuccessful) {
+                            val responseBodyString = it.body?.string()
 
-                        // Parsing the JSON response
-                        val json = JSONObject(responseBodyString)
-                        val result = json.getBoolean("result")
-                        val message = json.getString("message")
+                            // Parsing the JSON response
+                            val json = JSONObject(responseBodyString)
+                            val result = json.getBoolean("result")
+                            val message = json.getString("message")
 
-                        activity?.runOnUiThread {
-                            // Update your UI based on 'result' and 'message'
-                            if (result) {
-                                // Login success
-                                Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-                                findNavController().navigate(R.id.nav_home)
-
-
-                            } else {
-                                // Login failed, show the message to the user
-                                Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+                            activity?.runOnUiThread {
+                                // Update your UI based on 'result' and 'message'
+                                if (result) {
+                                    // Login success
+                                    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+                                    findNavController().navigate(R.id.nav_home)
+                                } else {
+                                    // Login failed, show the message to the user
+                                    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+                                }
                             }
-                        }
-                    } else {
-                        // Handle the case where the server responded with an error status
-                        activity?.runOnUiThread {
-                            // Update UI to show an error message
+                        } else {
+                            // Handle the case where the server responded with an error status
+                            activity?.runOnUiThread {
+                                // Update UI to show an error message
+                            }
                         }
                     }
                 }
-            }
-        })
+            })
+
+        } else {
+            // Show error message
+            Toast.makeText(requireContext(), "Revise su Conexión a Internet", Toast.LENGTH_SHORT).show()
+        }
 
 
     }
