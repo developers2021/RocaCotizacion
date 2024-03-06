5a41e9ab64cd07d3d94c88b80d36f43d2f8ca445
Norman Henriquez <84880453+developers2021@users.noreply.github.com>
2024-03-06 09:32:55 -0600

    Cambiando nombre
    Cambiando nombre de Cotizaciones a Pedidos, quitando el navigation drawer, para usarlo despues

--- app/src/main/java/com/example/rocacotizacion/Utilidades.kt
+++ app/src/main/java/com/example/rocacotizacion/Utilidades.kt
@@ -3,7 +3,7 @@ package com.example.rocacotizacion
 class Utilidades {
     companion object {
         //pruebas
-        const val URL_WEB_SERVICE = "https://a19d-2803-7780-6-5380-54c6-ec47-645f-e21d.ngrok-free.app"
+        const val URL_WEB_SERVICE = "https://ae55-2803-7780-6-5380-8c07-37a9-80c0-7930.ngrok-free.app"
         const val URL_AUTH_LOGIN = "$URL_WEB_SERVICE/UserAuth"
     }
 }
