deb984c8f89960768411d162d3e3aaa03bf1dc0d
Norman Henriquez <84880453+developers2021@users.noreply.github.com>
2024-10-18 09:07:44 -0600

    Cambios que Fueron realizados antes de tomar posesion del proyecto por Jerry

--- app/src/main/java/com/example/rocacotizacion/Utilidades.kt
+++ app/src/main/java/com/example/rocacotizacion/Utilidades.kt
@@ -5,7 +5,7 @@ class Utilidades {
         //pruebas
        //const val URL_WEB_SERVICE = "https://0968-45-166-93-218.ngrok-free.app"
         //produccion
-        const val URL_WEB_SERVICE = "https://87cc-2803-7780-6-5380-38f0-2403-1f8c-bc34.ngrok-free.app"
+        const val URL_WEB_SERVICE = "https://preventaapimovil.azurewebsites.net"
         const val URL_AUTH_LOGIN = "$URL_WEB_SERVICE/UserAuth"
         const val URL_SAVE_JSON_PEDIDO = "$URL_WEB_SERVICE/SaveJsonPedido"
 
